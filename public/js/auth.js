df1a0859c644ea5c7f310a25cfe3f1da2445dfea
Olivier MICHAUD <o.michaud@servicemagic.eu>
2017-11-26 17:10:19 +0100

    hapiserver ok

--- public/js/auth.js
+++ public/js/auth.js
@@ -15,18 +15,18 @@
 		var _rightNotConnected, _currentIdentity, authService, _authenticated = 0;
 
 		authService = {
-			userToken : '' 
+			userToken : ''
 		};
-		
+
 		authService.isIdentityResolved = function() {
 			return angular.isDefined(_currentIdentity);
 		};
-		
+
 		authService.isAuthenticated = function() {
 			//console.log('_authenticated',_authenticated);
 			return _authenticated;
 		};
-		
+
 		authService.isInAnyRights = function(rights) {
 			var i, j;
 			rights = angular.isArray(rights) ? rights : [ rights ];
@@ -40,7 +40,7 @@
 			for (j = 0; j < rights.length; j++) if (_currentIdentity.config.rightSa[rights[j]]) return true;
 			return false;
 		};
-		
+
 		authService.removeAuthenticate = function() {
 			_currentIdentity = null;
 			_authenticated = null;
@@ -52,20 +52,20 @@
 			authService.userToken = token;
 			$http.defaults.headers.common["Authorization"] = "Bearer " + token
 		}
-		
+
 		authService.setTokenId = function(token) {
 			authService.initTokenId(token);
 			localStorageService.set("token", token);
 		};
-		
+
 		authService.getTokenId = function() {
 			return localStorageService.get("token");
 		};
-		
+
 		authService.clearTokenId = function() {
 			return localStorageService.remove("token");
 		};
-		
+
 		authService.checkAndLoadIdentity = function() {
 			var deferred = $q.defer();
 			if (angular.isDefined(_currentIdentity)) {
@@ -79,7 +79,7 @@
 					authService.initTokenId(token);
 					$http({
 					method : "GET",
-					url : "auth/check"
+					url : "/api/users/info"
 				}).success(function(res) {
 					authService.setIdentity(token)
 					deferred.resolve(_currentIdentity);
@@ -91,17 +91,17 @@
 			}
 			return deferred.promise;
 		};
-		
+
 		authService.getCurrentIdentity = function() {
 			return _currentIdentity;
 		};
-		
+
 		authService.login = function (credentials) {
 			_currentIdentity = null;
 			_authenticated = false;
 			return $http({
 				method: "POST",
-				url: "auth/login",
+				url: "api/auth/login",
 				data: credentials
 			}).then(function(res) {
 				var deferred = $q.defer();
@@ -122,18 +122,18 @@
 				return deferred.promise;
 			});
 		};
-		
+
 		authService.logout = function() {
 			_currentIdentity = null;
 			$rootScope.$broadcast('authentication:logout', _currentIdentity);
 			_authenticated = false;
 			authService.clearTokenId();
 		};
-		
+
 		authService.isSessionAuthenticated = function() {
 			return $http({
 				method : "POST",
-				url : "auth/check"
+				url : "/api/users/info"
 			});
 		};
 
@@ -142,7 +142,7 @@
 			console.log(_currentIdentity);
 			$rootScope.$broadcast('authentication:login', _currentIdentity);
 			_authenticated = true;
-			authService.setTokenId(token);	
+			authService.setTokenId(token);
 		};
 		return authService;
 	}])
@@ -152,7 +152,7 @@
 				return authService.checkAndLoadIdentity().then(function() {
 					var isAuthenticated = authService.isAuthenticated();
 					$rootScope.toState.data = $rootScope.toState.data || {};
-					if ($rootScope.toState.data.rights && $rootScope.toState.data.rights.length > 0 && !authService.isInAnyRights($rootScope.toState.data.rights)){ 
+					if ($rootScope.toState.data.rights && $rootScope.toState.data.rights.length > 0 && !authService.isInAnyRights($rootScope.toState.data.rights)){
 						if (isAuthenticated) {
 							$state.go("accessdenied")
 						}
@@ -211,7 +211,7 @@
 			errorMessage :'',
 			rememberCredentials : false
 		};
-		
+
 		var cred = localStorageService.get("credentials");
 		$scope.credentials = cred ? cred : {
 			username: "",
@@ -220,18 +220,18 @@
 		$scope.checkMail = function(mail) {
 			return new RegExp("^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,10}$").test(mail);
 		};
-		
+
 		$scope.login = function (credentials) {
 			$scope.data.message='';
 			$scope.data.errorMessage='';
-			if (!credentials.username || !credentials.password){ 
+			if (!credentials.username || !credentials.password){
 				$scope.data.errorMessage='formulaire incomplet';
 			}else {
 				credentials.username = credentials.username.toLowerCase();
 				authService
 				.login(credentials)
 				.then(function(data) {
-					if ($scope.data.rememberCredentials && credentials.username && credentials.password){ 
+					if ($scope.data.rememberCredentials && credentials.username && credentials.password){
 						localStorageService.set("credentials", {
 							username: credentials.username,
 							password: credentials.password
@@ -251,11 +251,11 @@
 				});
 			}
 		};
-		
+
 		$scope.register = function(credentials) {
 			$scope.data.message='';
 			$scope.data.errorMessage='';
-			if (!credentials.username || !credentials.password){ 
+			if (!credentials.username || !credentials.password){
 				$scope.data.errorMessage='formulaire incomplet';
 			}else {
 				credentials.username = credentials.username.toLowerCase();
